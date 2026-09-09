85aa621bf47e2c93bbb6660b7d9b2540c745f411
UltraInstinctSri <srijan.majumder.9dec@gmail.com>
2026-09-09 22:20:39 +0530

    New files added

---
+++ python/exception_handling.py
@@ -0,0 +1,14 @@
+astr = 'Hello'
+try:
+    istr = int(astr)
+except :
+    istr = -1
+print('First' , istr)
+
+astr = '123'
+try:
+    istr = int(astr)
+except :
+    istr = -1
+
+print ('Second', istr)
